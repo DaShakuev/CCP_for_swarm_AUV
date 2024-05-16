9a3389d8196e2f10ec019f07a6bc815f5668e777
DaShakuev <dan.shakuev@yandex.ru>
2024-05-16 19:04:33 +0300

    testcommit

--- test0.py
+++ test0.py
@@ -1,16 +1,20 @@
-from area import Area
-from grid import Grid
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
 
-vertices1 = [(5, 10), (10, 15), (20,  20), (15, 5), (1, 1)]
-vertices2 = [(2, 18), (8, 19), (14, 15), (17, 8), (5, 3)]
-vertices3 = [(1, 6), (4, 18), (10, 20), (16, 15), (18, 7)]
-vertices4 = [(3, 12), (7, 18), (14, 19), (18, 11), (12, 6)]
-vertices5 = [(2, 8), (8, 18), (12, 20), (16, 12), (6, 5)]
+vertices = [(5, 10), (10, 15), (20,  20), (15, 5), (1, 1)]
 gs = 0.9
-grid_size = 25
 
-Area(vertices1, gs, grid_size)
-Area(vertices2, gs, grid_size)
-Area(vertices3, gs, grid_size)
-Area(vertices4, gs, grid_size)
-Area(vertices5, gs, grid_size)
+# Создаем экземпляр фигуры и оси
+fig, ax = plt.subplots()
+
+# Настройка пределов осей
+ax.set_xlim(min(x for x, y in vertices) - 1, max(x for x, y in vertices) + 1)
+ax.set_ylim(min(y for x, y in vertices) - 1, max(y for x, y in vertices) + 1)
+
+ax.grid(True)
+
+# Создаем многоугольник с помощью массива вершин
+polygon = Polygon(vertices, closed=True, fill=None, edgecolor='b')
+
+# Добавляем многоугольник на оси
+ax.add_patch(polygon)
